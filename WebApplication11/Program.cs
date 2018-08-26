e26a7d35c330586cf907c51048dc6cf5faa7d9ad
pauliusnorkus <paulius.nrk@gmail.com>
2018-08-26 21:45:58 +0300

    a

--- WebApplication11/Program.cs
+++ WebApplication11/Program.cs
@@ -19,6 +19,9 @@ namespace WebApplication11
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options => {
+                    options.Listen(System.Net.IPAddress.Loopback, 5080); //HTTP port
+                })
                 .UseStartup<Startup>();
     }
 }
